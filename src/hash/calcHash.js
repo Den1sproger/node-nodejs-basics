d66f86a939637b4e6ca7eaa4a35bfb2e362f488e
Denis <strigodenis@gmail.com>
2024-01-29 13:26:42 +0300

    feat: implement hash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import crypto from 'crypto';
+import fs from 'fs';
+
+
 const calculateHash = async () => {
-    // Write your code here 
-};
+  const filePath = 'src/hash/files/fileToCalculateHashFor.txt';
+  const stream = fs.createReadStream(filePath, 'utf-8');
+  
+  stream.on("data", (chunk) => {
+    const hash = crypto.createHash('sha256').update(chunk).digest('hex');
+    console.log(hash);
+  });
+}
 
 await calculateHash();
